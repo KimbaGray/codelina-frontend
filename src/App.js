613bca93ac432c79c1ab46f849f63ad6c51b8637
Kim Gray <kim.lc.gray@gmail.com>
2020-01-11 12:58:04 +0000

    Created homepage and menu component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,56 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
+import Heroimage from "./images/Heroimage.jpg";
+import Carolina from "./images/Carolina.jpg";
+import Menu from "./components/menu";
 
-function App() {
-  return <div className="App">This is the Codelina frontend</div>;
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <nav>
+          <Menu />
+        </nav>
+        <div className="aboutCarolina">
+          <img src={Heroimage} alt="Carolina doing yoga" />
+          <h1>About Carolina</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed finibus
+            tincidunt efficitur. Cras pharetra blandit dignissim. Nulla dui sem,
+            finibus eget nunc id, fringilla aliquam ante. Suspendisse semper
+            pharetra ipsum, vitae porta turpis ornare a.
+          </p>
+          <button type="button" className="btn btn-link">
+            Contact Carolina
+          </button>
+        </div>
+        <div className="aboutReiki">
+          <img src={Carolina} alt="Carolina in sun" />
+          <h1>About Reiki</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed finibus
+            tincidunt efficitur. Cras pharetra blandit dignissim. Nulla dui sem,
+            finibus eget nunc id, fringilla aliquam ante. Suspendisse semper
+            pharetra ipsum, vitae porta turpis ornare a. Fusce non convallis
+            tellus, quis mattis quam. Phasellus sagittis elit vel urna
+            pellentesque vehicula.
+          </p>
+        </div>
+        <div className="bookASession">
+          <h1>Book your Reiki session</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed finibus
+            tincidunt efficitur.
+          </p>
+          <button type="button" className="btn btn-link">
+            Book your session here
+          </button>
+        </div>
+        <footer>To do - footer component</footer>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import "bootstrap/dist/css/bootstrap.min.css";
+import $ from "jquery";
+import Popper from "popper.js";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
