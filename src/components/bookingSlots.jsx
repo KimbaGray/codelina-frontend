bbc8fcd956850388b4b4c7a91e4e8162ecc0d374
Andreea <andreeaioanabarbu@gmail.com>
2020-02-06 21:08:18 +0000

    Fixed PayPal component, styled PP button, display product name and date in PP confirmation

--- src/components/bookingSlots.jsx
+++ src/components/bookingSlots.jsx
@@ -15,23 +15,23 @@ class BookingSlots extends Component {
   state = {
     slots: [
       {
-        startTime: "2020-01-16 21:09:00Z",
-        endTime: "2020-01-16 22:09:00Z",
+           startTime: "2020-02-14 22:00:00Z",
+        endTime: "2020-02-14 23:30:00Z",
         service: "Reiki"
       },
       {
-        startTime: "2020-01-16 10:09:00Z",
-        endTime: "2020-01-16 12:09:00Z",
+        startTime: "2020-02-16 10:00:00Z",
+        endTime: "2020-02-16 11:00:00Z",
         service: "Reiki"
       },
       {
-        startTime: "2020-08-22 10:08:00Z",
-        endTime: "2020-08-22 12:52:00Z",
+        startTime: "2020-02-17 10:00:00Z",
+        endTime: "2020-02-17 11:00:00Z",
         service: "Reiki"
       },
       {
-        startTime: "2020-09-22 10:52:00Z",
-        endTime: "2020-09-22 11:52:00Z",
+             startTime: "2020-02-17 22:00:00Z",
+        endTime: "2020-02-17 23:45:00Z",
         service: "Reiki"
       }
     ]
