ef6f73b4b3b8c3906d6944eb1c540fd15ef41580
Kim Gray <kim.lc.gray@gmail.com>
2020-03-19 20:56:20 +0000

    final css changes and corrected auto email

--- src/components/bookingSlots.jsx
+++ src/components/bookingSlots.jsx
@@ -1,68 +1,83 @@
-import React, { Component } from 'react';
-import BookingSlot from './bookingSlot';
-import Axios from 'axios';
+import React, { Component } from "react";
+import BookingSlot from "./bookingSlot";
+import Axios from "axios";
 
-const api = 'http://brazilianglow.co.uk/api/availability';
+const api = "http://brazilianglow.co.uk/api/availability";
 
 function groupBy(data, keyGenerator) {
-	return data.reduce(function(acc, item) {
-		const key = keyGenerator(item);
-		const group = acc[key] || [];
-		group.push(item);
-		acc[key] = group;
-		return acc;
-	}, {});
+  return data.reduce(function(acc, item) {
+    const key = keyGenerator(item);
+    const group = acc[key] || [];
+    group.push(item);
+    acc[key] = group;
+    return acc;
+  }, {});
 }
 
 class BookingSlots extends Component {
-	state = {
-		slots: []
-	};
+  state = {
+    slots: []
+  };
 
-	componentDidMount() {
-		this.getData();
-	}
+  componentDidMount() {
+    this.getData();
+  }
 
-	getData() {
-		Axios.get(api).then((result) => {
-			console.log(result.data);
-			this.setState({ slots: result.data });
-		});
-	}
+  getData() {
+    Axios.get(api).then(result => {
+      console.log(result.data);
+      this.setState({ slots: result.data });
+    });
+  }
 
-	render() {
-		const groupedSlots = groupBy(this.state.slots, (slot) =>
-			new Date(slot.slotStartTime).toLocaleDateString('en', {
-				weekday: 'short',
-				day: 'numeric',
-				month: 'short'
-			})
-		);
-		return (
-			<div>
-				<br />
-				<p>£60/hour</p>
-				<br />
-				<p>Please select your session below and pay the 20% deposit via the PayPal button</p>
-				<br />
-				{Object.entries(groupedSlots).map(([ date, slots ]) => (
-					<div>
-						<h1>{date}</h1>
-						<br />
-						{slots.map((slot) => (
-							<BookingSlot startTime={slot.slotStartTime} service={'Reiki'} endTime={slot.slotEndTime} />
-						))}
-					</div>
-				))}
-				<div>
-					<br />
-					<p>
-						By making this payment you are putting a deposit on the full cost of a Reiki session. You also
-						agree to the <a href="">Terms and Conditions</a> and the processing of your data.
-					</p>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    const groupedSlots = groupBy(this.state.slots, slot =>
+      new Date(slot.slotStartTime).toLocaleDateString("en", {
+        weekday: "short",
+        day: "numeric",
+        month: "short"
+      })
+    );
+    return (
+      <div>
+        <br />
+        <p className="money">£60/hour</p>
+        <br />
+        <p className="money">
+          Please select your session below and pay the 20% deposit via the
+          PayPal button
+        </p>
+        <br />
+        {Object.entries(groupedSlots).map(([date, slots]) => (
+          <div>
+            <h1>{date}</h1>
+            <br />
+            {slots.map(slot => (
+              <BookingSlot
+                startTime={slot.slotStartTime}
+                service={"Reiki"}
+                endTime={slot.slotEndTime}
+              />
+            ))}
+          </div>
+        ))}
+        <div>
+          <br />
+          <p>
+            By making this payment you are putting a deposit on the full cost of
+            a Reiki session. You also agree to the{" "}
+            <a
+              href="https://drive.google.com/file/d/17Y4fbJYh5tPjetwgUsMl-VtwTmooINO1/view?usp=sharing"
+              target="_blank"
+              className="tAndCs"
+            >
+              Terms and Conditions
+            </a>{" "}
+            and the processing of your data.
+          </p>
+        </div>
+      </div>
+    );
+  }
 }
 export default BookingSlots;
--- src/components/signin.jsx
+++ src/components/signin.jsx
@@ -1,80 +1,86 @@
-import React, { Component } from 'react';
-import Menu from './menu';
-import Axios from 'axios';
+import React, { Component } from "react";
+import Menu from "./menu";
+import Axios from "axios";
 
-const api = 'http://brazilianglow.co.uk/api/users/login';
+const api = "http://brazilianglow.co.uk/api/users/login";
 
 class SignIn extends Component {
-	state = {
-		emailVar: '',
-		passwordVar: ''
-	};
+  state = {
+    emailVar: "",
+    passwordVar: ""
+  };
 
-	postData = (e) => {
-		e.preventDefault();
-		//console.log(this.state.emailVar, this.state.passwordVar);
-		Axios.post(api, {
-			email: this.state.emailVar,
-			password: this.state.passwordVar
-		}).then((result) => {
-			console.log(result);
-			if (result.data > 0) {
-				localStorage.setItem('token', result.data);
-				localStorage.setItem('email', this.state.emailVar);
+  postData = e => {
+    e.preventDefault();
+    //console.log(this.state.emailVar, this.state.passwordVar);
+    Axios.post(api, {
+      email: this.state.emailVar,
+      password: this.state.passwordVar
+    }).then(result => {
+      console.log(result);
+      if (result.data > 0) {
+        localStorage.setItem("token", result.data);
+        localStorage.setItem("email", this.state.emailVar);
 
-				console.log('signin', localStorage.getItem('token'));
-				this.props.onLoggedIn(this.state.emailVar);
-			} else {
-				console.log('You have entered the wrong credentials');
-			}
-		});
-	};
+        console.log("signin", localStorage.getItem("token"));
+        this.props.onLoggedIn(this.state.emailVar);
+      } else {
+        console.log("You have entered the wrong credentials");
+      }
+    });
+  };
 
-	onEmailChange = (event) => {
-		this.setState({
-			emailVar: event.target.value
-		});
-	};
+  onEmailChange = event => {
+    this.setState({
+      emailVar: event.target.value
+    });
+  };
 
-	onPasswordChange = (event) => {
-		this.setState({
-			passwordVar: event.target.value
-		});
-	};
+  onPasswordChange = event => {
+    this.setState({
+      passwordVar: event.target.value
+    });
+  };
 
-	render() {
-		return (
-			<div>
-				<h1>Log in</h1>
-				<div className="form-group">
-					<label htmlFor="exampleInputEmail1">Email address</label>
-					<input
-						value={this.state.emailVar}
-						onChange={this.onEmailChange}
-						type="email"
-						className="form-control"
-						id="exampleInputEmail1"
-						aria-describedby="emailHelp"
-						placeholder="Enter email"
-					/>
-				</div>
-				<div className="form-group">
-					<label htmlFor="exampleInputPassword1">Password</label>
-					<input
-						value={this.state.passwordVar}
-						onChange={this.onPasswordChange}
-						type="password"
-						className="form-control"
-						id="exampleInputPassword1"
-						placeholder="Password"
-					/>
-				</div>
-				<button type="submit" className="btn customButton" onClick={this.postData}>
-					Submit
-				</button>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <h1 className="money">Log in</h1>
+        <div className="form-group">
+          <label htmlFor="exampleInputEmail1">Email address</label>
+          <input
+            value={this.state.emailVar}
+            onChange={this.onEmailChange}
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            placeholder="Enter email"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="exampleInputPassword1">Password</label>
+          <input
+            value={this.state.passwordVar}
+            onChange={this.onPasswordChange}
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+            placeholder="Password"
+          />
+        </div>
+        <div className="signInButton">
+          <button
+            type="submit"
+            className="btn customButton"
+            onClick={this.postData}
+          >
+            Submit
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default SignIn;
